26f78b331e85571551ea15001fc959d4ee6613d2
Zain <learnerstudent001@gmail.com>
2024-05-30 16:39:35 +0500

    Context API

---
+++ 8-ContextAPI/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import React from 'react'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login'
+import Profile from './components/Profile'
+
+
+function App() {
+  return (
+    <UserContextProvider>
+      <h1>Context API learning | Summer Holidays @ Zain Ali</h1>
+      <Login/>
+      <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
---
+++ 8-ContextAPI/src/assets/components/Login.jsx
@@ -0,0 +1,9 @@
+import React, {useState, useContext} from "react";
+
+export default function Login(){
+    return(
+        <div>
+
+        </div>
+    )
+}
